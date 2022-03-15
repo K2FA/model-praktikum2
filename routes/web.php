a786b562a17684089e90e2b9efffb9c040aef45c
K2FA <khalidarrafi6331@gmail.com>
2022-03-15 16:14:34 +0700

    setting controller

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/index', [MenuController::class, 'index']);
+
+Route::get('/', [MenuController::class, 'index']);
 Route::get('/about', function () {
     return view('about');
 });
