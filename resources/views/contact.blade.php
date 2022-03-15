8a55f115ffe98ec6f4ecd12eb86f3a83d9ef709f
K2FA <khalidarrafi6331@gmail.com>
2022-03-15 14:44:19 +0700

    view setting

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.main')
+
+
+@section('main-content')
+
+	<div class="pageTitle">
+	<div class="container">
+	<h2>Contact Us</h2>
+	</div>
+	</div>
+
+	 <div class="container">
+	 <div class="bodyInner">
+			<p>
+			&nbsp;
+			</p>
+			<p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p>
+	</div>
+	</div>
+
+@endsection
+
+
+
+
+
+
+
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="{{asset('ico/favicon.ico')}}">
+    <title>Bootstrappage.com free templates</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/carousel.css')}}">
+
+
+
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+
+    @include('layouts.header')
+
+    @yield('main-content')
+
+    @include('layouts.content')
+
+    @include('layouts.footer')
+
+
+
+      <!-- /END THE FEATURETTES -->
+
+
+      <!-- FOOTER -->
+
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/holder.js')}}"></script>
+  </body>
+</html>
